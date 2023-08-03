4bc93f9521d49a0e8508b5682504a44556076037
Clayton Hughes <clayton@vthera.com>
2023-08-02 18:58:56 -0700

    Add wrapping ctor for ConfigurationBindException

--- ConfigurationExtensions/ConfigurationBindException.cs
+++ ConfigurationExtensions/ConfigurationBindException.cs
@@ -5,4 +5,8 @@ public class ConfigurationBindException : Exception
     public ConfigurationBindException(string message) : base(message)
     {
     }
+
+    public ConfigurationBindException(string message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
